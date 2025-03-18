6816887a1ecb8e9645db1a7872efd480fd8cec04
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:42:22 +0700

    feat: Add Student Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/testingmockmvc/entity/Student.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.testingmockmvc.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing-mock-mvc
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 18/03/25
+ * Time: 08.41
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class Student {
+    private Integer id;
+    private String name;
+}
