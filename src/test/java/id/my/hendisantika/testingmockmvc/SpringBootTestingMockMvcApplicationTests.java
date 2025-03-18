8d3ee89262bbce644eaa450154a5a2a578ff44fd
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:39:49 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/testingmockmvc/SpringBootTestingMockMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.testingmockmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingMockMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
