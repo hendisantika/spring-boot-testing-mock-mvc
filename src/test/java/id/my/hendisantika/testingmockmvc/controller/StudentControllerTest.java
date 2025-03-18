b102379afc3d5b1ec681e370443fa12bdf21d855
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:51:52 +0700

    feat: Add StudentControllerTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.testingmockmvc.controller;
+
+import id.my.hendisantika.testingmockmvc.SpringBootTestingMockMvcApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing-mock-mvc
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 18/03/25
+ * Time: 08.51
+ * To change this template use File | Settings | File Templates.
+ */
+@WebMvcTest
+@ContextConfiguration(classes = SpringBootTestingMockMvcApplication.class)
+class StudentControllerTest {
+
+}
