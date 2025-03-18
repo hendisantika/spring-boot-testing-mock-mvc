59280a9b1f122d0b13875684a5f10c52a0733c96
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:44:36 +0700

    feat: Add StudentService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/testingmockmvc/service/StudentService.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.testingmockmvc.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing-mock-mvc
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 18/03/25
+ * Time: 08.44
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class StudentService {
+}
