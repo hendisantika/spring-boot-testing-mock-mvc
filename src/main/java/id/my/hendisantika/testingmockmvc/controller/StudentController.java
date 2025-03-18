7e804b97dadc69d4c915d8537dda6c34223b1057
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:51:01 +0700

    feat: Add deleteExample in StudentController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/testingmockmvc/controller/StudentController.java
+++ src/main/java/id/my/hendisantika/testingmockmvc/controller/StudentController.java
@@ -6,10 +6,12 @@ import id.my.hendisantika.testingmockmvc.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -46,5 +48,11 @@ public class StudentController {
     public ResponseEntity<Student> putExample(@RequestBody Student student) {
         student = studentService.updateStudent(student);
         return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @DeleteMapping(URIConstant.DELETE_MAPPING)
+    public ResponseEntity<String> deleteExample(@RequestParam("student-id") String studentId) {
+        String response = studentService.deleteStudent(studentId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
