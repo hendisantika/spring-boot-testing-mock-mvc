c92b5ee8a31bfca65ca8807f9c8f517478b5ec11
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:47:59 +0700

    feat: Add URIConstant🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/testingmockmvc/constants/URIConstant.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.testingmockmvc.constants;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing-mock-mvc
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 18/03/25
+ * Time: 08.47
+ * To change this template use File | Settings | File Templates.
+ */
+public class URIConstant {
+    public static final String GET_MAPPING = "/getMapping";
+
+    public static final String POST_MAPPING = "/postMapping";
+
+    public static final String PUT_MAPPING = "/putMapping";
+
+    public static final String DELETE_MAPPING = "/deleteMapping";
+}
