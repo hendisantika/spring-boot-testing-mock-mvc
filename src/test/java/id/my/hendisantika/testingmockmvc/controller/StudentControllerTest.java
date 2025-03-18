a2d47341c2819203bc137dd6414e71038726c39b
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:53:40 +0700

    feat: Add testPostExample in StudentControllerTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
+++ src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
@@ -7,16 +7,19 @@ import id.my.hendisantika.testingmockmvc.entity.Student;
 import id.my.hendisantika.testingmockmvc.service.StudentService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.http.RequestEntity.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,5 +54,18 @@ class StudentControllerTest {
         Mockito.when(studentService.getStudents()).thenReturn(students);
         mockMvc.perform(get(URIConstant.GET_MAPPING)).andExpect(status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(1)))
                 .andExpect(jsonPath("$[0].name", Matchers.equalTo("Yuji")));
+    }
+
+    @Test
+    public void testPostExample() throws Exception {
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Yuji");
+        Mockito.when(studentService.saveStudent(ArgumentMatchers.any())).thenReturn(student);
+        String json = mapper.writeValueAsString(student);
+        mockMvc.perform(post(URIConstant.POST_MAPPING).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+                        .content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.name", Matchers.equalTo("Yuji")));
     }
 }
