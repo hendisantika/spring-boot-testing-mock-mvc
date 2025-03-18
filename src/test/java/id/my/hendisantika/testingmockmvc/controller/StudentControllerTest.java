da195a23ef722bb26df0ee901d9d8a002e7417c5
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:54:00 +0700

    feat: Add testPutExample in StudentControllerTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
+++ src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.http.RequestEntity.post;
+import static org.springframework.http.RequestEntity.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -67,5 +68,18 @@ class StudentControllerTest {
                         .content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id", Matchers.equalTo(1)))
                 .andExpect(jsonPath("$.name", Matchers.equalTo("Yuji")));
+    }
+
+    @Test
+    public void testPutExample() throws Exception {
+        Student student = new Student();
+        student.setId(2);
+        student.setName("Geto");
+        Mockito.when(studentService.updateStudent(ArgumentMatchers.any())).thenReturn(student);
+        String json = mapper.writeValueAsString(student);
+        mockMvc.perform(put(URIConstant.PUT_MAPPING).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+                        .content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.equalTo(2)))
+                .andExpect(jsonPath("$.name", Matchers.equalTo("Geto")));
     }
 }
