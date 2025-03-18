8a8db4f859475fbb24b7908e8ac190083b9c1b5b
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 08:53:14 +0700

    feat: Add testGetExample in StudentControllerTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
+++ src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
@@ -1,8 +1,25 @@
 package id.my.hendisantika.testingmockmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import id.my.hendisantika.testingmockmvc.SpringBootTestingMockMvcApplication;
+import id.my.hendisantika.testingmockmvc.constants.URIConstant;
+import id.my.hendisantika.testingmockmvc.entity.Student;
+import id.my.hendisantika.testingmockmvc.service.StudentService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +35,21 @@ import org.springframework.test.context.ContextConfiguration;
 @WebMvcTest
 @ContextConfiguration(classes = SpringBootTestingMockMvcApplication.class)
 class StudentControllerTest {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    private MockMvc mockMvc;
+    @Mock
+    private StudentService studentService;
 
+    @Test
+    public void testGetExample() throws Exception {
+        List<Student> students = new ArrayList<>();
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Yuji");
+        students.add(student);
+        Mockito.when(studentService.getStudents()).thenReturn(students);
+        mockMvc.perform(get(URIConstant.GET_MAPPING)).andExpect(status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].name", Matchers.equalTo("Yuji")));
+    }
 }
