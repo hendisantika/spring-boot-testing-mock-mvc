2069c7da58ed6d14165cbde4d30294d5a416a932
hendisantika <hendisantika@yahoo.co.id>
2025-04-20 21:58:22 +0700

    fix: Unit Test🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
+++ src/test/java/id/my/hendisantika/testingmockmvc/controller/StudentControllerTest.java
@@ -16,13 +16,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.http.RequestEntity.post;
-import static org.springframework.http.RequestEntity.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -43,8 +43,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = SpringBootTestingMockMvcApplication.class)
 class StudentControllerTest {
     private static final ObjectMapper mapper = new ObjectMapper();
+
     @Autowired
     private MockMvc mockMvc;
+
     @Mock
     private StudentService studentService;
 
@@ -56,7 +58,9 @@ class StudentControllerTest {
         student.setName("Yuji");
         students.add(student);
         Mockito.when(studentService.getStudents()).thenReturn(students);
-        mockMvc.perform(get(URIConstant.GET_MAPPING)).andExpect(status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(1)))
+        mockMvc.perform(get(URIConstant.GET_MAPPING))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
                 .andExpect(jsonPath("$[0].name", Matchers.equalTo("Yuji")));
     }
 
@@ -67,8 +71,12 @@ class StudentControllerTest {
         student.setName("Yuji");
         Mockito.when(studentService.saveStudent(ArgumentMatchers.any())).thenReturn(student);
         String json = mapper.writeValueAsString(student);
-        mockMvc.perform(post(URIConstant.POST_MAPPING).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
-                        .content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
+
+        mockMvc.perform(MockMvcRequestBuilders.post(URIConstant.POST_MAPPING)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.getBytes(StandardCharsets.UTF_8))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id", Matchers.equalTo(1)))
                 .andExpect(jsonPath("$.name", Matchers.equalTo("Yuji")));
     }
@@ -80,8 +88,11 @@ class StudentControllerTest {
         student.setName("Geto");
         Mockito.when(studentService.updateStudent(ArgumentMatchers.any())).thenReturn(student);
         String json = mapper.writeValueAsString(student);
-        mockMvc.perform(put(URIConstant.PUT_MAPPING).contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
-                        .content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+        mockMvc.perform(MockMvcRequestBuilders.put(URIConstant.PUT_MAPPING)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.getBytes(StandardCharsets.UTF_8))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", Matchers.equalTo(2)))
                 .andExpect(jsonPath("$.name", Matchers.equalTo("Geto")));
     }
